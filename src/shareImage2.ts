22a0cda27b6fda12dba2a6b41e1490fb978f638b
Roman Nguyen <march08@outlook.com>
2023-12-19 14:09:23 +0100

    use media responsive url

--- src/shareImage2.ts
+++ src/shareImage2.ts
@@ -12,47 +12,36 @@ const getCanvasBlob = (canvas: HTMLCanvasElement) => {
   });
 };
 
-const getTriggerElementId = () => {
-  const existingTriggerEl = document.getElementById(TRIGGER_EL_ID);
+const initiateDownload = (canvas: HTMLCanvasElement, username: string) => {
+  const triggerEl =
+    document.getElementById(TRIGGER_EL_ID) || document.createElement("a");
 
-  console.log("existingTriggerEl", existingTriggerEl);
-
-  if (existingTriggerEl) {
-    return existingTriggerEl as HTMLAnchorElement;
-  } else {
-    const triggerEl = document.createElement("a");
-    triggerEl.id = TRIGGER_EL_ID;
-    document.body.appendChild(triggerEl);
-    triggerEl.style.display = "none";
-    return triggerEl as HTMLAnchorElement;
+  if (triggerEl.getAttribute("data-sharable-initialized") === "true") {
+    triggerEl.click();
+    return;
   }
-};
 
-const initiateDownload = (
-  canvas: HTMLCanvasElement,
-  triggerEl: HTMLAnchorElement,
-  username: string
-) => {
   const image = canvas.toDataURL(BLOB_TYPE);
 
+  triggerEl.id = TRIGGER_EL_ID;
   triggerEl.setAttribute("href", image);
   triggerEl.setAttribute("download", `vsco_snapshot_2023_${username}`);
   triggerEl.setAttribute("data-sharable-initialized", "true");
   triggerEl.click();
 };
 
-let isGenerating = false;
 let storedCanvas: HTMLCanvasElement | undefined = undefined;
+let canvasBlob: Blob | undefined = undefined;
 
 const shareWithNavigator = async (
   canvas: HTMLCanvasElement,
   filename: string
 ) => {
   if (navigator.canShare && /Android|iPhone/i.test(navigator.userAgent)) {
-    const blob = await getCanvasBlob(canvas);
-    if (blob) {
+    canvasBlob = canvasBlob || (await getCanvasBlob(canvas));
+    if (canvasBlob) {
       const filesArray = [
-        new File([blob], filename, {
+        new File([canvasBlob], filename, {
           type: BLOB_TYPE,
           lastModified: new Date().getTime(),
         }),
@@ -60,13 +49,9 @@ const shareWithNavigator = async (
       const shareData = {
         title: "My VSCO Snapshot 2023",
         files: filesArray,
+        url: window.location.href,
       };
 
-      console.log("shareData", shareData);
-      console.log(
-        "navigator.canShare(shareData)",
-        navigator.canShare(shareData)
-      );
       if (navigator.canShare(shareData)) {
         console.log("click", shareData);
         navigator
@@ -75,16 +60,12 @@ const shareWithNavigator = async (
             console.log("DOWNLOADED");
           })
           .catch((e) => {
-            console.log("share err", e);
-            console.log("Not allowed", e.name === "NotAllowedError");
             if (e.name === "NotAllowedError") {
               throw e;
             }
           });
       }
-
-      isGenerating = false;
-      return;
+      throw new Error("Cannot use navigator.share");
     }
   }
 
@@ -95,19 +76,6 @@ export const shareAsset = async (
   data: SearchParamKeyValue,
   args: VscoSnapshotConfig
 ) => {
-  const triggerEl = getTriggerElementId();
-
-  if (isGenerating) {
-    console.log("Image generator running");
-    return;
-  }
-
-  if (triggerEl.getAttribute("data-sharable-initialized") === "true") {
-    console.log("ALREADY GENERATED");
-    triggerEl.click();
-    return;
-  }
-
   if (!storedCanvas) {
     storedCanvas =
       document.getElementById(CANVAS_ID) ||
@@ -116,15 +84,11 @@ export const shareAsset = async (
 
   if (storedCanvas) {
     const filename = `vsco_snapshot_2023_${data.username}.jpg`;
-
-    // if above checks fails
-
     try {
       await shareWithNavigator(storedCanvas, filename);
     } catch (e) {
       console.log("eee", e);
-      initiateDownload(storedCanvas, triggerEl, data.username);
+      initiateDownload(storedCanvas, data.username);
     }
-    isGenerating = false;
   }
 };
